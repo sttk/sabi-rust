cbda989aaf4ebf6a79ac934ab942e3c4ec5509d2
Takayuki Sato <sttk.xslet@gmail.com>
2024-12-04 22:29:26 +0900

    new: added Err (#2)

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+// Copyright (C) 2024 Takayuki Sato. All Rights Reserved.
+// This program is free software under MIT License.
+// See the file LICENSE in this distribution for more details.
+
+mod errs;
+pub use errs::Err;
