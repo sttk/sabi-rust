f57e98877ece84f025938fd82c2ebb19ee28260e
sttk <sttk.xslet@gmail.com>
2024-12-24 03:58:26 +0900

    new: added AsyncGroup

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,8 @@
 // This program is free software under MIT License.
 // See the file LICENSE in this distribution for more details.
 
+mod async_group;
 mod errs;
+
+pub use async_group::AsyncGroup;
 pub use errs::Err;
