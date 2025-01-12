cb1269a9e3ddb900f23b261bdf38e224b42ae579
sttk <sttk.xslet@gmail.com>
2025-01-12 16:23:57 +0900

    update!,wip: comprehensive revision

--- src/async_group.rs
+++ src/async_group.rs
@@ -1,4 +1,4 @@
-// Copyright (C) 2024 Takayuki Sato. All Rights Reserved.
+// Copyright (C) 2024-2025 Takayuki Sato. All Rights Reserved.
 // This program is free software under MIT License.
 // See the file LICENSE in this distribution for more details.
 
@@ -6,36 +6,20 @@ use std::collections::HashMap;
 use std::thread;
 
 use crate::errors;
-
 use crate::Err;
 
-/// Executes added functions asynchronously.
+/// Manages multiple asynchronous operations and wait for all of them to complete.
 ///
-/// This trait is used as an argument of DaxSrc#setup, DaxConn#commit, DacConn#rollback, and
-/// DaxConn#forceback.
-pub trait AsyncGroup {
-    /// Adds and starts to run a target function.
-    fn add(&mut self, func: fn() -> Result<(), Err>);
-}
-
-pub(crate) struct AsyncGroupAsync<'a> {
+/// If an error occurs during any of these operations, it collects and stores the errors for lator
+/// retrieval.
+pub struct AsyncGroup<'a> {
     join_handles: Vec<thread::JoinHandle<Result<(), Err>>>,
     names: Vec<String>,
     pub(crate) name: &'a str,
 }
 
-impl AsyncGroup for AsyncGroupAsync<'_> {
-    fn add(&mut self, func: fn() -> Result<(), Err>) {
-        let handle = thread::spawn(move || match func() {
-            Ok(()) => Ok(()),
-            Err(err) => Err(err),
-        });
-        self.join_handles.push(handle);
-        self.names.push(self.name.to_string());
-    }
-}
-
-impl AsyncGroupAsync<'_> {
+impl AsyncGroup<'_> {
+    /// Creates a new `AsyncGroup`.
     pub(crate) fn new() -> Self {
         Self {
             join_handles: Vec::new(),
@@ -44,7 +28,26 @@ impl AsyncGroupAsync<'_> {
         }
     }
 
-    pub(crate) fn wait(&mut self, err_map: &mut HashMap<String, Err>) {
+    /// Adds a new asynchronous operation to this instance.
+    ///
+    /// The operation is provided as a function that returns an `Err`.
+    /// If the function encounters an error, it is recorded internally.
+    ///
+    /// # Parameters
+    /// - `func`: A function that will be executed in a separate thread. It must return a
+    ///   `Result<(), Err>`, where `Err` indicates a failure.
+    pub fn add(&mut self, func: fn() -> Result<(), Err>) {
+        let handle = thread::spawn(move || match func() {
+            Ok(()) => Ok(()),
+            Err(err) => Err(err),
+        });
+        self.join_handles.push(handle);
+        self.names.push(self.name.to_string());
+    }
+
+    pub(crate) fn join(&mut self) -> HashMap<String, Err> {
+        let mut err_map = HashMap::<String, Err>::new();
+
         while self.join_handles.len() > 0 {
             let name = self.names.remove(0);
             match self.join_handles.remove(0).join() {
@@ -70,207 +73,93 @@ impl AsyncGroupAsync<'_> {
                 }
             }
         }
-    }
-}
 
-pub(crate) struct AsyncGroupSync {
-    pub(crate) err: Option<Err>,
-}
-
-impl AsyncGroup for AsyncGroupSync {
-    fn add(&mut self, func: fn() -> Result<(), Err>) {
-        match func() {
-            Ok(_) => {}
-            Err(err) => {
-                self.err = Some(err);
-            }
-        }
-    }
-}
-
-impl AsyncGroupSync {
-    pub(crate) fn new() -> Self {
-        Self { err: None }
+        err_map
     }
 }
 
 #[cfg(test)]
 mod tests_async_group {
     use super::*;
+    use std::thread::*;
+    use std::time;
 
-    mod tests_async_group_async {
-        use super::*;
-        use std::thread;
-        use std::time;
+    #[test]
+    fn zero() {
+        let mut ag = AsyncGroup::new();
 
-        #[test]
-        fn when_zero_function() {
-            let mut ag = AsyncGroupAsync::new();
-            let mut hm = HashMap::new();
-            ag.wait(&mut hm);
-            assert_eq!(hm.len(), 0);
-        }
-
-        #[test]
-        fn when_one_function() {
-            let mut ag = AsyncGroupAsync::new();
-            ag.name = "foo";
-            ag.add(|| Ok(()));
-            let mut hm = HashMap::new();
-            ag.wait(&mut hm);
-            assert_eq!(hm.len(), 0);
-        }
-
-        #[test]
-        fn when_two_function() {
-            let mut ag = AsyncGroupAsync::new();
-            ag.name = "foo";
-            ag.add(|| {
-                thread::sleep(time::Duration::from_millis(20));
-                Ok(())
-            });
-            ag.name = "bar";
-            ag.add(|| {
-                thread::sleep(time::Duration::from_millis(10));
-                Ok(())
-            });
-            let mut hm = HashMap::new();
-            ag.wait(&mut hm);
-            assert_eq!(hm.len(), 0);
-        }
-
-        #[derive(Debug, PartialEq)]
-        enum Reasons {
-            BadNumber(u32),
-            BadString(String),
-        }
-
-        #[test]
-        fn when_one_function_and_error() {
-            let mut ag = AsyncGroupAsync::new();
-            ag.name = "foo";
-            ag.add(|| Err(Err::new(Reasons::BadNumber(123u32))));
-            let mut hm = HashMap::new();
-            ag.wait(&mut hm);
-            assert_eq!(hm.len(), 1);
-            assert_eq!(
-                *(hm.get("foo").unwrap().reason::<Reasons>().unwrap()),
-                Reasons::BadNumber(123u32)
-            );
-        }
-
-        #[test]
-        fn when_two_function_and_error() {
-            let mut ag = AsyncGroupAsync::new();
-            ag.name = "foo";
-            ag.add(|| {
-                thread::sleep(time::Duration::from_millis(20));
-                Err(Err::new(Reasons::BadNumber(123u32)))
-            });
-            ag.name = "bar";
-            ag.add(|| {
-                thread::sleep(time::Duration::from_millis(10));
-                Err(Err::new(Reasons::BadString("hello".to_string())))
-            });
-            let mut hm = HashMap::new();
-            ag.wait(&mut hm);
-            assert_eq!(hm.len(), 2);
-            assert_eq!(
-                *(hm.get("foo").unwrap().reason::<Reasons>().unwrap()),
-                Reasons::BadNumber(123u32)
-            );
-            assert_eq!(
-                *(hm.get("bar").unwrap().reason::<Reasons>().unwrap()),
-                Reasons::BadString("hello".to_string())
-            );
-        }
-
-        #[test]
-        fn when_three_function_and_panicked() {
-            let mut ag = AsyncGroupAsync::new();
-            ag.name = "foo";
-            ag.add(|| {
-                thread::sleep(time::Duration::from_millis(20));
-                panic!("panic 1");
-            });
-            let mut hm = HashMap::new();
-            ag.wait(&mut hm);
-            assert_eq!(hm.len(), 1);
-
-            match hm
-                .get("foo")
-                .unwrap()
-                .reason::<errors::AsyncGroup>()
-                .unwrap()
-            {
-                errors::AsyncGroup::ThreadPanicked { message } => assert_eq!(message, "panic 1"),
-            }
-        }
+        let m = ag.join();
+        assert_eq!(m.len(), 0);
     }
 
-    mod tests_async_group_sync {
-        use super::*;
+    #[test]
+    fn ok() {
+        let mut ag = AsyncGroup::new();
 
-        #[test]
-        fn when_zero_function() {
-            let ag = AsyncGroupSync::new();
-            assert!(ag.err.is_none());
-        }
+        ag.name = "foo";
+        ag.add(|| Ok(()));
+        let m = ag.join();
+        assert_eq!(m.len(), 0);
+    }
 
-        #[test]
-        fn when_one_function() {
-            let mut ag = AsyncGroupSync::new();
-            ag.add(|| Ok(()));
-            assert!(ag.err.is_none());
-        }
+    #[derive(Debug, PartialEq)]
+    enum Reasons {
+        BadNumber(u32),
+        BadString(String),
+        BadFlag(bool),
+    }
 
-        #[test]
-        fn when_two_function() {
-            let mut ag = AsyncGroupSync::new();
-            ag.add(|| Ok(()));
+    #[test]
+    fn error() {
+        let mut ag = AsyncGroup::new();
 
-            fn func() -> Result<(), Err> {
-                Ok(())
-            }
-            ag.add(func);
-            assert!(ag.err.is_none());
-        }
+        ag.name = "foo";
+        ag.add(|| {
+            thread::sleep(time::Duration::from_millis(50));
+            Err(Err::new(Reasons::BadNumber(123u32)))
+        });
 
-        #[test]
-        fn when_one_function_and_error() {
-            let mut ag = AsyncGroupSync::new();
-            ag.add(|| Err(Err::new("async error.".to_string())));
-            assert!(ag.err.is_some());
+        let m = ag.join();
+        assert_eq!(m.len(), 1);
+        assert_eq!(
+            format!("{:?}", *(m.get("foo").unwrap())),
+            "sabi::Err { reason = sabi::async_group::tests_async_group::Reasons BadNumber(123) }"
+        );
+    }
 
-            match ag.err.unwrap().reason::<String>() {
-                Ok(s) => assert_eq!(s, "async error."),
-                Err(_) => panic!(),
-            }
-        }
+    #[test]
+    fn multiple_errors() {
+        let mut ag = AsyncGroup::new();
 
-        #[test]
-        fn when_two_functions_and_error() {
-            let mut ag = AsyncGroupSync::new();
-            ag.add(|| Ok(()));
-            assert!(ag.err.is_none());
+        ag.name = "foo0";
+        ag.add(|| {
+            thread::sleep(time::Duration::from_millis(200));
+            Err(Err::new(Reasons::BadNumber(123u32)))
+        });
+        ag.name = "foo1";
+        ag.add(|| {
+            thread::sleep(time::Duration::from_millis(400));
+            Err(Err::new(Reasons::BadString("hello".to_string())))
+        });
+        ag.name = "foo2";
+        ag.add(|| {
+            thread::sleep(time::Duration::from_millis(800));
+            Err(Err::new(Reasons::BadFlag(true)))
+        });
 
-            #[derive(Debug)]
-            enum Reasons {
-                BadNumber(u32),
-            }
-
-            fn fail() -> Result<(), Err> {
-                Err(Err::new(Reasons::BadNumber(123)))
-            }
-            ag.add(fail);
-            assert!(ag.err.is_some());
-
-            match ag.err.unwrap().reason::<Reasons>() {
-                Ok(r) => match r {
-                    Reasons::BadNumber(n) => assert_eq!(*n, 123u32),
-                },
-                Err(_) => panic!(),
-            }
-        }
+        let m = ag.join();
+        assert_eq!(m.len(), 3);
+        assert_eq!(
+            format!("{:?}", *(m.get("foo0").unwrap())),
+            "sabi::Err { reason = sabi::async_group::tests_async_group::Reasons BadNumber(123) }"
+        );
+        assert_eq!(
+            format!("{:?}", *(m.get("foo1").unwrap())),
+            "sabi::Err { reason = sabi::async_group::tests_async_group::Reasons BadString(\"hello\") }"
+        );
+        assert_eq!(
+            format!("{:?}", *(m.get("foo2").unwrap())),
+            "sabi::Err { reason = sabi::async_group::tests_async_group::Reasons BadFlag(true) }"
+        );
     }
 }
